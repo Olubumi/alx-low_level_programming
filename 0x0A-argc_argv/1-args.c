eab4d2f01c7df053a401cc0d226e6e44fdb4ec7a
Isaac <mesoleisaac@yahoo.com>
2023-03-11 09:14:01 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,23 +6,12 @@
 
 /**
  * main - print the number of arguments passed to the program
-
  * @argc: number of arguments
-
  * @argv: array of arguments
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
-
-
-/**
- * main - Entry point
- *
- */
 int main(int argc, char *argv[])
 
 {
