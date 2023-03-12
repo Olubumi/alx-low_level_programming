b14399bef6b72274ee0703108f4cb7e2dd80c1d6
Isaac <mesoleisaac@yahoo.com>
2023-03-12 13:16:40 -0700

    UPDATE

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,21 +3,20 @@
 #include "main.h"
 
 
-
-/**
-
- * main - prints all arguments it receives
-
+ /**
+ * main - print all arguments it receives
  * @argc: number of arguments
-
  * @argv: array of arguments
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
+/**
+ * main - print all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 
 {
