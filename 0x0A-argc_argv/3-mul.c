ebfbf328b45deda9efd355fef21ea3a3aca9f6c1
Isaac <mesoleisaac@yahoo.com>
2023-03-13 02:37:02 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,17 @@
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
-i = 0;
+
+	i = 0;
+
 	d = 0;
+
 	n = 0;
+
 	len = 0;
+
 	f = 0;
+
 	digit = 0;
 	while (s[len] != '\0')
 		len++;
@@ -47,18 +53,27 @@ i = 0;
  *
  * Return: 0 (Success), 1 (Error)
  */
+
 int main(int argc, char *argv[])
 {
+
 	int result, num1, num2;
+
 	if (argc < 3 || argc > 3)
+
 	{
 		printf("Error\n");
+
 		return (1);
+
 	}
 	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
-	return (0);
 
+	num2 = _atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
