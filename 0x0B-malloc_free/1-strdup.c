ee01f3baf2fccc0eab9f4aa2842e844a6b002259
Isaac <mesoleisaac@yahoo.com>
2023-03-13 04:08:49 -0700

    Update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,45 +2,24 @@
 		
 #include <stdlib.h>
 		
-/**
-		
- * create_array - create array of size size and assign char c
-		
- * @size: size of array
-		
- * @c: char to assign
-		
- * Description: creat array of size size and assign char c
-		
- * Return: pointer to array, NULL if fail
-		
- *
-		
- */
-		
-char *create_array(unsigned int size, char c)
-		
+char *_strdup(char *str)
 {
-		
-	char *str;
-		
-	unsigned int i;
-		
+  int i = 0, j, k = 0;
+  char *newStr = NULL;
 
-		
-	str = malloc(sizeof(char) * size);
-		
-	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
+  while (str[k] != '\0')
+    {
+      i++;
+      k++;
+    }
 
-		
-	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);
-		
+  newStr = malloc(sizeof(char) * (i + 1));
+
+  if (str == NULL)
+    return (NULL);
+
+  for (j = 0; j <= i; j++)
+    newStr[j] = str[j];
+
+  return (newStr);
 }
-
