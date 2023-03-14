07fa876ae04167809a4c670b3f5835bb9111532f
Isaac <mesoleisaac@yahoo.com>
2023-03-14 07:54:42 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,48 +1,73 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * string_nconcat - function to concatnate strings with n bytes
- * @s1: destination for concatnation
- * @s2: source of string
- * @n: int type for size of byte
- * Return: pointer to new memory allocated
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+int count_word(char *s)
 {
-	int count, count1;
-	int sign = n;
-	char *ptr;
-	int len1, len2;
+	int flag, c, w;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	flag = 0;
+	w = 0;
+	for (c = 0; s[c] != '\0'; c++)
+{
+	if (s[c] == ' ')
+	flag = 0;
+	else if (flag == 0)
+{
+	flag = 1;
+	w++;
+}
+}
+	return (w);
+}
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
 
-	if (sign >= len2)
+char **strtow(char *str)
+
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+	len++;
+	words = count_word(str);
+	if (words == 0)
+	return (NULL);
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+	return (NULL);
+	for (i = 0; i <= len; i++)
 	{
-		sign = len2;
-		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-	}
-	else
-		ptr = malloc(sizeof(char) * (len1 + n + 1));
-	if (ptr == NULL)
-		return (NULL);
-	for (count = 0; count < len1; count++)
+	if (str[i] == ' ' || str[i] == '\0')
 	{
-		ptr[count] = s1[count];
-	}
-	for (count1 = 0; count1 < sign; count1++)
+	if (c)
 	{
-		ptr[count++] = s2[count1];
+	end = i;
+	tmp = (char *) malloc(sizeof(char) * (c + 1));
+	if (tmp == NULL)
+	return (NULL);
+	while (start < end)
+	*tmp++ = str[start++];
+	*tmp = '\0';
+	matrix[k] = tmp - c;
+	k++;
+	c = 0;
+		}
 	}
-	ptr[count++] = '\0';
-	return (ptr);
+	else if (c++ == 0)
+	start = i;
+}
+	matrix[k] = NULL;
+	return (matrix);
 }
